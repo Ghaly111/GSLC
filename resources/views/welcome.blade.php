423f26a999b8fed2e38c7d1780e7923e6ae66e94
Ghaly Zahirsyah <76165037+Ghaly111@users.noreply.github.com>
2022-11-05 18:26:44 +0700

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @extends('template')
+
+    @section('title', 'GHALZ RESTO')
+
+    @section('content')
+        <head>
+            <div class="text-center" style="height: 465px">
+                <button><a href="/home">Home</a></button>
+            </div>
+        </body>
+    @endsection
+</html>
